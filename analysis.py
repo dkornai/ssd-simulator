440311c7d8585cc495f6fb937c32a37b55f3c087
dkornai <danielkornai@gmail.com>
2023-12-05 17:56:29 +0000

    collecting results for entity types seperately now

--- analysis.py
+++ analysis.py
@@ -3,9 +3,17 @@ import numpy as np
 class CmeSimResults():
     def __init__(self, results:np.ndarray):
         self.raw = results
-       
-        n_vars = results.shape[2]
+
+        # Collect basic information about the dimensionality of the resuls.
+        self.n_samples = results.shape[0]
+        self.n_vars = results.shape[1]
+        self.n_timepoints = results.shape[2]
 
         # Average counts across replicates
         self.average = np.nanmean(results, axis=0)
+
+        # Collect entity types seperately
+        self.e0_count = results[:,np.arange(0, self.n_vars, 2),:]
+        self.e1_count = results[:,np.arange(1, self.n_vars, 2),:]
+
 
